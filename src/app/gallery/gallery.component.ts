bac0a46986bec52b97bc69367cb270deaf705701
Xavier <548500@cognizant.com>
2017-09-22 18:17:49 +0530

    from office 22-9-17

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit {
 
   constructor() { }
- public data='gallery';
+ public parentData='gallery';
   ngOnInit() {
   }
 
